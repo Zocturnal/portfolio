a87841dc5b2e32ef8c3d4b5e2b488be18412329c
Zocturnal <zocturnal@gmail.com>
2024-11-25 20:59:59 -0600

    Foundation for Nav and Footer

---
+++ assets/javascript/index.js
@@ -0,0 +1,8 @@
+function hamburgerMenuToggle() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
